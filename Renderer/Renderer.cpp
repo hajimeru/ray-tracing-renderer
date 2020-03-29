85e0dc12013f6ea1e86ba6329b5f9c023fb495a4
Codenightcat <codenightcat@hotmail.com>
2020-03-30 00:11:21 +0800

    create and test Vector.cpp

--- Renderer/Renderer.cpp
+++ Renderer/Renderer.cpp
@@ -4,21 +4,20 @@
 #include <iostream>
 #include <string>
 #include "tgaimage.h"
-
+#include "Vector3.h"
 
 const char* IMAGE_URL = "test.tga";
 
 
 int main()
 {
-	TGAImage image(255, 255, TGAImage::RGB);
-	for (int i = 0; i < image.get_width(); ++i) {
-		for (int j = 0; j < image.get_height(); ++j) {
-			image.set(i, j, TGAColor(i, j, 0));
-		}
-	}
-	image.write_tga_file(IMAGE_URL);
-	
+	Vector3 t11(2, 2, 2);
+	Vector3 t12(3, 3, 3);
+	double length = t12.Length();
+	std::cout << t11.Dot(t12) << "\n";
+	std::cout << t12.Normalize() << "\n";
+
+
 }
 
 
