cb6afe5c474eb30ef592c0a9862d6f29b311067c
Codenightcat <codenightcat@hotmail.com>
2020-03-29 21:26:59 +0800

    输出图像测试

--- Renderer/Renderer.cpp
+++ Renderer/Renderer.cpp
@@ -1,21 +1,24 @@
 ﻿// Renderer.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
-#include "pch.h"
 #include <iostream>
+#include <string>
+#include "tgaimage.h"
+
+
+const char* IMAGE_URL = "test.tga";
+
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	TGAImage image(255, 255, TGAImage::RGB);
+	for (int i = 0; i < image.get_width(); ++i) {
+		for (int j = 0; j < image.get_height(); ++j) {
+			image.set(i, j, TGAColor(i, j, 0));
+		}
+	}
+	image.write_tga_file(IMAGE_URL);
+	
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
 
-// 入门提示: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
