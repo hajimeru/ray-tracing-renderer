748ac6b540c2b6802bbedd56ca437dc44d59adc6
Codenightcat <codenightcat@hotmail.com>
2020-03-30 14:06:29 +0800

    create PerspectiveCamera

---
+++ Renderer/PerspectiveCamera.h
@@ -0,0 +1,14 @@
+#ifndef _PERSPECTIVE_CAMERA_H_
+#define _PERSPECTIVE_CAMERA_H_
+
+#include "Vector3.h"
+
+class PerspectiveCamera {
+
+
+private:
+	Vector3
+};
+
+#endif // !_PERSPECTIVE_CAMERA_H_
+
