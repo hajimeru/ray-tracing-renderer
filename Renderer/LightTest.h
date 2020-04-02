da71949e12678380fe121985d4fefe7b72e10551
Codenightcat <codenightcat@hotmail.com>
2020-04-02 20:47:48 +0800

    implement SpotLight

--- Renderer/LightTest.h
+++ Renderer/LightTest.h
@@ -14,6 +14,7 @@
 #include "Ray3.h"
 #include "Render.h"
 #include "Sphere.h"
+#include "SpotLight.h"
 #include "tgaimage.h"
 #include "UnionGeometry.h"
 #include "Vector3.h"
@@ -168,6 +169,76 @@ void PointLightTest()
 	image.write_tga_file(IMAGE_URL);
 }
 
+
+void SpotLightTest()
+{
+	const char* IMAGE_URL = "SpotLightTest.tga";
+	TGAImage image(WIDTH, HEIGHT, TGAImage::RGB);
+
+	//Camera
+	Vector3 eye(0, 1, 3);
+	Vector3 front(0, 0, -1);
+	Vector3 up(0, 1, 0);
+	PerspectiveCamera camera(eye, front, up, 90, W_DIVIDE_H);
+
+
+	// Two spheres
+	auto sphere1 = make_shared<Sphere>(Vector3(-1, 1, 0), 1);
+	auto sphere2 = make_shared<Sphere>(Vector3(1, 1, 0), 1);
+
+	sphere1->setMaterial(make_shared<PhongMaterial>(Color(1, 0, 0), Color::WHITE, 10, 0.25));
+	sphere2->setMaterial(make_shared<PhongMaterial>(Color(0.5, 0.5, 0.5), Color::WHITE, 16, 0.25));
+
+	//plane
+	auto plane1 = make_shared<Plane>(Vector3(-1, 0, 0), -2);
+	auto plane2 = make_shared<Plane>(Vector3(0, 1, 0), 0);
+	auto plane3 = make_shared<Plane>(Vector3(0, 0, 1), -1);
+
+	plane1->setMaterial(make_shared<PhongMaterial>(Color(0.5, 0.5, 0.5), Color(1, 1, 1), 32, 0.25));
+	plane2->setMaterial(make_shared<PhongMaterial>(Color(0.5, 0.5, 0.5), Color(1, 1, 1), 32, 0.25));
+	plane3->setMaterial(make_shared<PhongMaterial>(Color(0.5, 0.5, 0.5), Color(1, 1, 1), 32, 0.25));
+
+	//plane2->setMaterial(make_shared<CheckerMaterial>(1, 0.25));
+
+	SpotLight light(Color(1,1,1)* 10,Vector3(0,3,3),Vector3(0,-1,-1),10,60,2,true);
+
+	shared_ptr<Geometry> scene = make_shared<UnionGeometry>(vector<shared_ptr<Geometry>>({ plane2, sphere1, sphere2 }));
+
+	for (int x = 0; x < WIDTH; ++x) {
+		double sx = x / (double)WIDTH;
+		for (int y = 0; y < HEIGHT; ++y) {
+			double sy = y / (double)HEIGHT;
+			Ray3 cmRay = camera.generateRay(sx, sy);
+			IntersectResult result = scene->Intersect(cmRay);
+			if (result.getGeometry()) {
+				Color c = Color::BLACK;
+
+				LightSample lightSample = light.Sample(scene, result.getPosition());
+				if (lightSample != LightSample::Zero) {
+					auto NdotL = result.getNormal().Dot(lightSample.L());
+					if (NdotL >= 0) {
+						c += lightSample.EL() *  NdotL;
+						//cout << c << "\n";
+					}
+				}
+
+				double r = MyMath::clip<double>(c.r, 0, 1);
+				double g = MyMath::clip<double>(c.g, 0, 1);
+				double b = MyMath::clip<double>(c.b, 0, 1);
+
+				c = Color(r, g, b);
+				TGAColor color(255, 255, 255);
+
+				color.bgra[0] *= c.b;
+				color.bgra[1] *= c.g;
+				color.bgra[2] *= c.r;
+				image.set(x, y, color);
+			}
+		}
+	}
+	image.flip_vertically();
+	image.write_tga_file(IMAGE_URL);
+}
 
 #endif // !_LIGHT_TEST_H_
 
--- Renderer/main.cpp
+++ Renderer/main.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-	PointLightTest();
+	SpotLightTest();
 }
 
 
